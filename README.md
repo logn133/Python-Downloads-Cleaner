5bd2c1c69eee1d3b262bf1e6a2f8e036ad4cc45c
logn133 <logan.cusano@gmail.com>
2018-12-02 18:57:21 -0500

    Finished, updating and optimizing

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Python-Downloads-Cleaner
-Cleans and organizes the windows downloads folder.
+
+##Installation
+Take this script and run it with the task scheduler in windows.
+
+##Usage
+To edit the script how you like, set the boolean `debug` to True so no files will be moved or deleted when you run it(make sure you set this back to True when you're done so the script can actually work).
+```Python
+debug = True
+```
+The other main boolean is `keepOldFiles`, set this to true if you want to move all old files to a folder instead of deleting them.
+```Python
+keepOldFiles = True
+```
+To change file types and edit folder names, edit the dictionary named `requiredDirs`, say you wanted to add a new folder for CAD files. All you have to do is add it to the end, so from this
+```Python
+requiredDirs = {"Programs":["exe","bat","msi"], "Pictures":["jpg","png","gif","bmp"], "Documents":["doc","docx","txt","pdf","pptx","ppt"], "ISOs":["bz2","zip","gz"], "Archives":["iso","img"], "OLD":[],"Installers":[], "Logs":["log"]}
+```
+to this
+```Python
+requiredDirs = {"Programs":["exe","bat","msi"], "Pictures":["jpg","png","gif","bmp"], "Documents":["doc","docx","txt","pdf","pptx","ppt"], "ISOs":["bz2","zip","gz"], "Archives":["iso","img"], "OLD":[],"Installers":[], "Logs":["log"], "CAD-Files":["cad","ipt"]}
+```
+and Bob's your uncle, you're set!
--- cleaner.py
+++ cleaner.py
@@ -1,88 +1,78 @@
 #Take all 'exe' files and put them into a labeled folder, remove any non picture file older than X days/weeks/months, put all pictures into a labeled folder
-#ToDo make the lists updatable, make the lists stored in a resource file to be parsed at startup
 import os
 import shutil
+from datetime import datetime, timedelta
+
+
 #Initializing global vars
-requiredDirs = ["Programs", "Pictures", "Documents", "ISOs"]
-
-progCheck = False
-executableFiles = ["exe","bat"]
-progPath = os.path.expanduser('~/Downloads/Programs/')
-
-docCheck = False
-documentFiles = ["doc","docx","txt","pdf"]
-docPath = os.path.expanduser('~/Downloads/Documents/')
-
-imgCheck = False
-imageFiles = ["iso","img"]
-imgPath = os.path.expanduser('~/Downloads/ISOs')
-
-picCheck = False
-pictureFiles = ["jpg","png","gif"]
-picPath = os.path.expanduser('~/Downloads/Pictures')
-
+requiredDirs = {"Programs":["exe","bat","msi"], "Pictures":["jpg","png","gif","bmp"], "Documents":["doc","docx","txt","pdf","pptx","ppt"], "ISOs":["bz2","zip","gz"], "Archives":["iso","img"], "OLD":[],"Installers":[], "Logs":["log"]}
+toDelete = []
 path = os.path.expanduser('~/Downloads/')
+
+#Set to True for testing
+debug = False
+
+#Set to true if you don't want your old files being deleted
+keepOldFiles = True
 
 def dirCheck():
     #Checks to see if the folders required are present
-    global progCheck, picCheck
+    global requiredDirs
     dirs = os.listdir(path)
-    for dir in dirs:
-        if dir in requiredDirs:
-            if dir in requiredDirs[0]:
-                progCheck = True
-                print("Programs exsist")
-            if dir in requiredDirs[1]:
-                picCheck = True
-                print("Programs exsist")
-            if dir in requiredDirs[2]:
-                docCheck = True
-                print("Programs exsist")
-            if dir in requiredDirs[4]:
-                isoCheck = True
-                print("Programs exsist")
-    if not progCheck:
-        print("Programs doesn't exsist, making it now...")
-        os.mkdir(path+"Programs")
-    if not picCheck:
-        print("Pictures doesn't exsist, making it now...")
-        os.mkdir(path+"Pictures")
-    if not docCheck:
-        print("Pictures doesn't exsist, making it now...")
-        os.mkdir(path+"Documents")
-    if not isoCheck:
-        print("Pictures doesn't exsist, making it now...")
-        os.mkdir(path+"ISOs")
+    for dir in requiredDirs:
+        if not dir in dirs:
+            print(dir+" doens't exsist, making it now...\n")
+            os.mkdir(path + dir)
+        else:
+            print("All directories are present...\n")
+
+def checkDate(fullFilePath):
+    global toDelete
+    try:
+        created = os.path.getmtime(fullFilePath)
+    except:
+        print("An error occured, trying creation date...\n")
+        try:
+            created = os.path.getctime(fullFilePath)
+        except:
+            print("Another error man, it's broke I don't know...\nSetting datetime to '0'\n")
+            created = 0
+    monthAgo = datetime.utcnow() - timedelta(days=30)
+    fileUTC = datetime.utcfromtimestamp(created)
+    if fileUTC < monthAgo:
+        if not debug:
+            toDelete.append(fullFilePath)
+        return True
+    elif fileUTC > monthAgo:
+        return False
 
 def scanDir():
-    dirInfo = os.walk(path)
-    for fullPath, dirName, files in dirInfo:
-        for filename in files:
-            extention = filename[-6:]
-            extention = extention.split(".",1)[-1]
-            created = os.path.getmtime(fullPath+"/"+files)
-            if extention in executableFiles:
-                print("its a bat")
-                shutil.move(fullpath+filename, progPath)
-            if extention in pictureFiles:
-                print("its a pic")
-                shutil.move(fullpath+filename, picPath)
-            if extention in imageFiles:
-                print("its an image file")
-                shutil.move(fullpath+filename, isoPath)
-            if extention in documentFiles:
-                print("its a document")
-                shutil.move(fullpath+filename, docPath)
+    global toDelete, requiredDirs, removeFiles
+    dirInfo = os.listdir(path)
+    for filename in dirInfo:
+        fullFilePath = path + "/" + filename
+        extention = filename.rsplit(".",1)[-1]
+        for dir in requiredDirs:
+            if extention in requiredDirs[dir]:
+                if not debug:
+                    if dir == "Programs" and "install" in filename or dir == "Programs" and "Install" in filename:
+                        print("\n"+filename+" is an installer and will be moved to the 'Installers' directory...\n")
+                        shutil.move(fullFilePath, path+"Installers")
+                    else:
+                        print("\n"+filename+" is a " + extention + " and will be moved to the '"+ dir + "' directory...\n")
+                        shutil.move(fullFilePath, path+dir)
+        if checkDate(fullFilePath):
+            print("\n" + filename + " is older than a month, it has been set for removal...\n")
 
-
+def deleteOld():
+    for x in toDelete:
+        if not debug:
+            if keepOldFiles:
+                shutil.move(x, path+"OLD/")
+            elif not keepOldFiles:
+                os.remove(x)
 
 #Program
 dirCheck()
 scanDir()
-
-
-#def checkDate():
-
-#def removeOld():
-
-#def moveFiles():
+deleteOld()
